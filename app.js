423bebb1a2f62527e59479c529241c9df6a48808
Kontanin <boss12853@gmail.com>
2023-05-22 11:39:38 +0700

    frist1

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('E-Commerce API');
+const express=require('express')
+
+const app =express()
+const port =process.env.port||3000
+const start =(port)=>{
+    try{
+        app.listen(port)
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+start()
+
