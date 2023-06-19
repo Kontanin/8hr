724c9bc0ac11be344a431855869e73de5ae8ce6c
Kontanin <boss12853@gmail.com>
2023-06-19 21:39:22 +0700

    edit

--- errors/index.js
+++ errors/index.js
@@ -2,7 +2,9 @@ const CustomAPIError = require('./custom-api');
 const UnauthenticatedError = require('./unauthenticated');
 const NotFoundError = require('./not-found');
 const BadRequestError = require('./bad-request');
+const UnauthorizedError =require('./unauthorized')
 module.exports = {
+  UnauthorizedError,
   CustomAPIError,
   UnauthenticatedError,
   NotFoundError,
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,13 +1,30 @@
 const CustomError =require('../errors')
 const {isTokenValid} = require('../utils')
 
-const authenticationUser =async(req,res,next) => {
+const authenticateUser =async(req,res,next) => {
     const token= req.signedCookies.token;
     if(!token) {
-        console.log('error , no token present');
+        throw new CustomError.UnauthenticatedError('Authentication Invalid');
     }
-    console.log('tok')
+    try{
+        const {name,userId,role} =isTokenValid({});
+        req.user= {name: user.name , userId:user._id , role:user.role};
+        next();
+    }
+    catch(error){
+        throw new CustomError.UnauthenticatedError('Authentication Invalid');
+    }
     next();
 }
 
-module.exports={authenticationUser}
+const authorizePermissions=(req,res,next)=>{
+    if(req.user.role!=='admin'){
+        throw new CustomError.UnauthorizedError('uthentication Invalid only admin')
+    next();
+
+}}
+
+module.exports= {
+      authenticateUser,authorizePermissions
+};
+
